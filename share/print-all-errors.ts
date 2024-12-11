c4012541380ea8246c7c6158cefaf3814fccc3d4
takker99 <37929109+takker99@users.noreply.github.com>
2024-12-11 12:22:12 +0900

    fix: print-all-errors

--- share/print-all-errors.ts
+++ share/print-all-errors.ts
@@ -1,12 +1,3 @@
-import hasDom from "has-dom";
-import process from "node:process";
-
-if (hasDom()) {
-  globalThis.addEventListener("unhandledrejection", (reason) => {
-    console.error("Unhandled Rejection", reason);
-  });
-} else {
-  process.on("unhandledRejection", (err) => {
-    console.error("Unhandled Rejection", err.stack || err);
-  });
-}
+globalThis.addEventListener("unhandledrejection", (reason) => {
+  console.error("Unhandled Rejection", reason);
+});
